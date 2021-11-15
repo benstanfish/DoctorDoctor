1db3d5f92da5f56a84029c37dafa32254e933a42
benstanfish <benstanfish@hotmail.com>
2021-11-14 18:48:45 -0800

    Additional changes

--- DoctorDoctor/DoctorForm.Designer.cs
+++ DoctorDoctor/DoctorForm.Designer.cs
@@ -32,10 +32,10 @@
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.propertyGrid2 = new System.Windows.Forms.PropertyGrid();
-            this.treeView2 = new System.Windows.Forms.TreeView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonClose
@@ -51,38 +51,36 @@
             // 
             // propertyGrid1
             // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.propertyGrid1.HelpVisible = false;
-            this.propertyGrid1.Location = new System.Drawing.Point(231, 12);
+            this.propertyGrid1.Location = new System.Drawing.Point(231, 45);
             this.propertyGrid1.Name = "propertyGrid1";
             this.propertyGrid1.PropertySort = System.Windows.Forms.PropertySort.NoSort;
-            this.propertyGrid1.Size = new System.Drawing.Size(594, 170);
+            this.propertyGrid1.Size = new System.Drawing.Size(1002, 142);
             this.propertyGrid1.TabIndex = 1;
             this.propertyGrid1.ToolbarVisible = false;
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Location = new System.Drawing.Point(12, 45);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(213, 567);
+            this.treeView1.Size = new System.Drawing.Size(213, 583);
             this.treeView1.TabIndex = 2;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // propertyGrid2
             // 
+            this.propertyGrid2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.propertyGrid2.HelpVisible = false;
-            this.propertyGrid2.Location = new System.Drawing.Point(231, 188);
+            this.propertyGrid2.Location = new System.Drawing.Point(231, 193);
             this.propertyGrid2.Name = "propertyGrid2";
-            this.propertyGrid2.Size = new System.Drawing.Size(594, 391);
+            this.propertyGrid2.Size = new System.Drawing.Size(1002, 435);
             this.propertyGrid2.TabIndex = 3;
             this.propertyGrid2.ToolbarVisible = false;
-            // 
-            // treeView2
-            // 
-            this.treeView2.Location = new System.Drawing.Point(831, 12);
-            this.treeView2.Name = "treeView2";
-            this.treeView2.Size = new System.Drawing.Size(290, 567);
-            this.treeView2.TabIndex = 4;
-            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterSelect);
             // 
             // button2
             // 
@@ -97,7 +95,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 585);
+            this.button3.Location = new System.Drawing.Point(12, 634);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(101, 29);
             this.button3.TabIndex = 6;
@@ -107,7 +105,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 620);
+            this.button4.Location = new System.Drawing.Point(12, 669);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(101, 29);
             this.button4.TabIndex = 7;
@@ -115,15 +113,23 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1221, 27);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // DoctorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1245, 710);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.treeView2);
             this.Controls.Add(this.propertyGrid2);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.propertyGrid1);
@@ -132,6 +138,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -141,9 +148,9 @@
         private PropertyGrid propertyGrid1;
         private TreeView treeView1;
         private PropertyGrid propertyGrid2;
-        private TreeView treeView2;
         private Button button2;
         private Button button3;
         private Button button4;
+        private TextBox textBox1;
     }
 }
--- DoctorDoctor/DoctorForm.cs
+++ DoctorDoctor/DoctorForm.cs
@@ -27,6 +27,8 @@ namespace DoctorDoctor
             //ProjNet projNet = ProjNet.ReadFromFile(ProjNetPath);
 
             propertyGrid1.SelectedObject = pn.DoctorChecks;
+            propertyGrid1.HorizontalScroll.Enabled = true;
+
 
             treeView1.Nodes.Clear();
             List<string> Disciplines = pn.GetDisciplines();
@@ -58,61 +60,16 @@ namespace DoctorDoctor
                 }
             }
 
-            //Debug.WriteLine(treeView1.Nodes.IndexOfKey("Electrical"));
-
-
-
-            //foreach (Comment comment in pn.Comments)
-            //{
-            //    treeView1.Nodes.Add(new TreeNode(comment.ToString()));
-            //    if (comment.Status.ToLower() == "open")
-            //    {
-            //        treeView1.Nodes[pn.Comments.IndexOf(comment)].ForeColor = Color.Red;
-            //        treeView1.Nodes[pn.Comments.IndexOf(comment)].Expand();
-            //    }
-
-            //    foreach(Evaluation evaluation in comment.Evaluations)
-            //    {
-            //        treeView1.Nodes[pn.Comments.IndexOf(comment)].Nodes.Add(new TreeNode(evaluation.Name));
-            //    }
-            //    foreach (Backcheck backcheck in comment.Backchecks)
-            //    {
-            //        treeView1.Nodes[pn.Comments.IndexOf(comment)].Nodes.Add(new TreeNode(backcheck.Name));
-            //    }
-            //}
-
-            //int l = 0;
-            //treeView2.Nodes.Clear();
-            //for(int i = 0; i < 2; i++)
-            //{
-            //    treeView2.Nodes.Add(l.ToString());
-            //    l++;
-            //    for(int j = 0; j < 2; j++)
-            //    {
-            //        treeView2.Nodes[i].Nodes.Add(l.ToString());
-            //        l++;
-            //        for(int k = 0; k < 2; k++)
-            //        {
-            //            treeView2.Nodes[i].Nodes[j].Nodes.Add(l.ToString());
-            //            l++;
-            //        }
-            //    }
-            //}
-
-            //treeView2.ExpandAll();
-
 
         }
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
-            //TODO: This button is a test method.
             this.Close();  
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            //propertyGrid2.SelectedObject = pn.Comments[treeView1.SelectedNode.Index];
             if(treeView1.SelectedNode != null)
             {
                 int level = treeView1.SelectedNode.Level;
@@ -153,6 +110,7 @@ namespace DoctorDoctor
 
                         break;
                     default:
+                        propertyGrid2.SelectedObject=null;
                         break;
                 }
             }
@@ -162,60 +120,6 @@ namespace DoctorDoctor
             }
         }
 
-        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            //int nodeIndex = treeView2.SelectedNode.Index;
-            //string parentIndex = "null";
-            //if (treeView2.SelectedNode.Parent != null)
-            //{
-            //    parentIndex = treeView2.SelectedNode.Parent.Index.ToString();
-            //}
-            //string report = $"Node at index of: {nodeIndex}\n" +
-            //                $"Node levels is: {treeView2.SelectedNode.Level.ToString()}\n" +
-            //                $"Parent node at index of: {parentIndex}";
-            //Debug.WriteLine(report);
-            //Debug.WriteLine(GetFamilyTree(treeView2.SelectedNode));
-            Debug.WriteLine(treeView2.SelectedNode.FullPath);
-        }
-
-        public static string GetFamilyTree(TreeNode n)
-        {
-            int index = n.Index;
-            int level = n.Level;
-            string s = string.Empty;
-            return s;
-
-            //switch (level)
-            //{
-            //    case 0:
-            //        s = $"Node index {index} at level {level}.\n";
-            //        break;
-            //    case 1:
-            //        s = $"Node index {index} at level {level}.\n" +
-            //            $"Parent index {n.Parent.Index} at level {n.Parent.Level}\n";
-            //        break;
-            //    default:
-            //        s = $"Node index {index} at level {level}.\n" +
-            //            $"Parent index {n.Parent.Index} at level {n.Parent.Level}\n" +
-            //            $"Grandparent index {n.Parent.Parent.Index} at level {n.Parent.Parent.Level}\n";
-            //        break;
-            //}
-
-            //string r = string.Empty;
-            //string[] heritage = new string[n.Level];
-            //for(int j = n.Level; j > 0; j--)
-            //{
-            //    r += ".Parent";
-            //    string getIndex = "n" + r + ".Index";
-            //    heritage[j] = $"{getIndex}";
-            //}
-
-
-            //return r = string.Join(",", heritage);
-
-
-            //return s;
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -234,6 +138,9 @@ namespace DoctorDoctor
             treeView1.CollapseAll();
         }
 
-
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }

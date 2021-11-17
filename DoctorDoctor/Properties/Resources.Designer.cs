fd83e00ce726834bbed707c8f164cef0a71e60ec
benstanfish <benstanfish@hotmail.com>
2021-11-16 23:54:05 -0800

    Fixed minor resx issues

--- DoctorDoctor/Properties/Resources.Designer.cs
+++ DoctorDoctor/Properties/Resources.Designer.cs
@@ -73,16 +73,6 @@ namespace DoctorDoctor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon doctor1 {
-            get {
-                object obj = ResourceManager.GetObject("doctor1", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
         internal static System.Drawing.Icon favicon {
             get {
                 object obj = ResourceManager.GetObject("favicon", resourceCulture);
